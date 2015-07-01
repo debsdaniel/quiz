b910f932ccfb1aa7e686e34ed668ede7295c2e85
Daniel Bojorge <debs.consultores@gmail.com>
2015-07-01 14:20:10 -0600

    Validación Agregar Error en Author

--- controllers/author_controller.js
+++ controllers/author_controller.js
@@ -1,4 +1,4 @@
 // GET /quizes/question
 exports.author = function(req, res) {
-   res.render('author', {encabezado: 'Bienvenidos a la sección del Autor'});
+   res.render('author', {encabezado: 'Bienvenidos a la sección del Autor', errors: []});
 };
